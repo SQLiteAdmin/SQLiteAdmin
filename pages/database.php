7a738d02124a7986c1e69840ce078e90f1c18a03
CodeWithSushil <sushilkumbhar77@gmail.com>
2024-05-22 20:22:25 +0530

    testing v0.0.2

---
+++ pages/database.php
@@ -0,0 +1,7 @@
+<div style="margin:auto;padding:1rem;">
+<h2>Create Database</h2>
+<form method="POST" action="/database">
+<input type="text" name="database" placeholder="Create Database." required>
+<button type="submit" name="create">Create</button>
+</form>
+</div>
