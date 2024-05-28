a0d1ff35d3c7a019226d7826c3be48268e3631b0
CodeWithSushil <sushilkumbhar77@gmail.com>
2024-05-28 20:44:06 +0530

    updated

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
+<?php
+define("APP_ROOT", __DIR__);
+
+require_once APP_ROOT."/vendor/autoload.php";
+
+?>
+<head>
+<link rel="stylesheet" href="./vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="./vendor/twbs/bootstrap-icons/font/bootstrap-icons.min.css">
+</head>
 <h2 align="center" style="color:red;"> Welcome to SQLite Admin by Sushil Kumar (Code With Sushil).</h2>
+<button class="btn btn-danger">Click <i></i></button>
